51badb613732d063fe5dd9ffb7cc90b6c65c26f7
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-04-22 23:14:38 +0800

    adding another file to run the other file and the final result

---
+++ run_transform.py
@@ -0,0 +1,6 @@
+from number_transformer import NumberTransformer
+
+number_transformer = NumberTransformer("twenty_integers.txt.txt")
+number_transformer.transform()
+
+print("Squares and Cubes files created successfully.")
